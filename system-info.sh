d3b657fe13bac7dc02a72cac15e5f97818f085c1
Harshit Bisht <117760947+bisht2311@users.noreply.github.com>
2026-08-18 05:13:58 +0530

    Fix typo in memory usage command

--- system-info.sh
+++ system-info.sh
@@ -10,6 +10,6 @@ echo "OS       : $(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"
 echo "============Top 5 disk usage==============="
 df -h --output=source,size,used,avail,pcent,target | sort -k5 -nr | head -5
 echo "==========Memory Usage==========="
-fee -h 
+free -h 
 echo "========== Top 5 CPU Processes =========="
 ps -eo pid,comm,%cpu --sort=-%cpu | head -6
